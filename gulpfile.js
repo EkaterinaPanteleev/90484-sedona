6997c8aa9897e2b14fe7b69970656be1b9a210c9
Ekaterina <panteleev.ekaterina@gmail.com>
2015-12-17 20:50:48 +0200

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,58 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var cmq = require("gulp-combine-mq");
+var minify = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var clean = require("gulp-clean");
+var imagemin = require("gulp-imagemin");
+
 
 gulp.task("style", function() {
-  return gulp.src("less/style.less")
+  return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer({browsers: "last 2 versions"})
     ]))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("/css"));
+});
+
+gulp.task("build", function() {
+    gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(cmq())
+    .pipe(postcss([
+      autoprefixer({browsers: "last 2 versions"})
+    ]))
+    .pipe(rename("style.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src(["source/img/**", "source/js/**", "source/index.html", "source/form.html", "source/post.html", "source/blog.html"], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("start", ["style"], function() {
-  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("source/less/**/*.less", ["style"]);
+});
+
+gulp.task("clean", function () {
+  return gulp.src("build/*", {read: false})
+    .pipe(clean());
+});
+
+gulp.task("compress", function() {
+  gulp.src("source/img/*")
+  .pipe(imagemin())
+  .pipe(gulp.dest("build/img"));
 });
 
 
